05a626341d81e21660a9d49b1cb3e8e0659c0504
Diego González <diego.gonzalez40740304@gmail.com>
2025-12-10 12:35:05 -0600

    (style) se agregaron los recursos de estilos

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function App() {
   return (
     <div>
-      <h1>Welcome to My Portfolio</h1>
+      <h1 className='text-3xl'>Welcome to My Portfolio</h1>
     </div>
   )
 }
